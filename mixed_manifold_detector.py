905f72cf9ddb533a7f4840da76bc0cd725863bd9
Di3g0E <114930111+Di3g0E@users.noreply.github.com>
2025-10-28 22:23:17 +0100

    update files

--- mixed_manifold_detector.py
+++ mixed_manifold_detector.py
@@ -1,13 +1,12 @@
 import argparse
 import os
 import time
-from sklearn.manifold import trustworthiness, TSNE, LocallyLinearEmbedding
+from sklearn.manifold import trustworthiness, TSNE
 
 from src.core.mixed_manifold_detector import MixedManifoldDetector
-from src.utils.io import save_experiment, load_dataset_csv
-from src.models.linear_autoencoder import LinearAutoencoder
-from src.models.linear_sparse_autoencoder import LinearSparseAutoencoder
 from src.models.denoising_sparse_autoencoder import DenoisingSparseAutoencoder
+from src.utils.io import save_experiment, load_dataset_csv
+
 
 def main():
     parser = argparse.ArgumentParser(description="MixedManifoldDetector CLI")
@@ -25,34 +24,25 @@ def main():
     # Crear instancias
     autoencoder = DenoisingSparseAutoencoder(input_dim=train_df.shape[1], epochs=5, sparsity_weight=1e-5, noise_weight=0.1)
     # manifold_algorithm = TSNE(n_components=2)
-    manifold_algorithm = LocallyLinearEmbedding(n_components=2, random_state=42)
+    manifold_algorithm = TSNE(n_components=2, random_state=42)
 
     detector = MixedManifoldDetector(autoencoder=autoencoder, manifold_algorithm=manifold_algorithm)
-    print("detector definido correctamete.")
     # Entrenamos el detector
     start_train = time.time()
     train_embedding = detector.fit_transform(train_df)
     train_time = time.time() - start_train
 
-    print("Train entrenado correctamente")
     
     if args.test_file:
         start_test = time.time()
         test_embedding = detector.fit_transform(test_df)
         test_time = time.time() - start_test
-        print("Test entrenado correctamente")
 
     # Calcular trustworthiness para train y test
     trustworthiness_train = trustworthiness(train_df, train_embedding)
     trustworthiness_test = trustworthiness(test_df, test_embedding)
 
-    print(trustworthiness_train, trustworthiness_test)
-
-    # Guardar datos
-    save_experiment(csv_path, args.train_file.split('.')[0]+"_experiment",
-                    trustworthiness_train, trustworthiness_test,
-                    train_time, test_time)
-    print("Guardado correctamente")
+    print(f"Resultados: Exp={args.train_file.split('.')[0]+'_experiment'}, Trust(Train)={trustworthiness_train:.4f}, Trust(Test)={trustworthiness_test:.4f}, T_Train={train_time:.2f}s, T_Test={test_time:.2f}s")
 
 if __name__ == "__main__":
     main()
